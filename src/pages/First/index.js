ceec83ef81448ba9a5c6738fe9634c6f515b4244
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2021-05-09 11:07:02 +0200

    adding caroussel on first page

--- src/pages/First/index.js
+++ src/pages/First/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import { Container, Divider, Header } from "semantic-ui-react";
 import "./first.css";
-import leSoft from "./le-soft.jpg";
+import leSoft1 from "./1.jpg";
+import leSoft2 from "./2.jpg";
+import leSoft3 from "./3.jpg";
+import leSoft4 from "./4.jpg";
+import leSoft5 from "./5.jpg";
 
 const First = ({ titleColor, textColor, productBackgroundColor }) => {
   return (
@@ -19,17 +25,40 @@ const First = ({ titleColor, textColor, productBackgroundColor }) => {
         Le Soft
       </Header>
       <Divider />
-      <img src={leSoft} alt="le soft" />
+      <Carousel showArrows={true} showThumbs={false} infiniteLoop={true}>
+        <div>
+          <img src={leSoft1} alt="etablissement" />
+        </div>
+        <div>
+          <img src={leSoft2} alt="burger" />
+        </div>
+        <div>
+          <img src={leSoft3} alt="chevre chaud" />
+        </div>
+        <div>
+          <img src={leSoft4} alt="poisson" />
+        </div>
+        <div>
+          <img src={leSoft5} alt="salade" />
+        </div>
+      </Carousel>
       <Divider />
       <Container
+        className="laius"
+        text
         style={{ color: textColor, background: productBackgroundColor }}
-        as="p"
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quibusdam
-        ipsum ut quia eaque, omnis earum dolore perferendis eligendi quod
-        asperiores quidem? Eveniet, dolorem voluptatibus, cum eius doloribus
-        beatae, veritatis laborum hic possimus quibusdam molestiae reiciendis
-        eos fugiat amet quia?
+        <p>Envie de ne rien faire de flemmarder, Pas de culpabilité,</p>
+        <p>
+          le Soft Napoléon est là pour réaliser toutes vos envies! Un petit
+          creux, une grande fringale, une détente de fin d'aprés midi, un moment
+          de "thé copines"
+        </p>
+        <p>
+          Nous sommes là Produits frais et fait maison, Chez nous la qualité est
+          toujours au Rendez-vous
+        </p>
+        <span className="author">Ludiwine</span>
       </Container>
     </Container>
   );
